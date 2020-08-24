d7d31f37beade338b79fc2424935823cf63d9a0a
flickz <omoyajowo2015@gmail.com>
2020-08-24 23:01:19 +0100

    implement modal functionality

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  let activeModalContent = '';
+
   function accordionHandler () {
     $(this).find("button[js-toggle='icon']").toggleClass('active')
     $(this).find("div[js-data='faqAnswer']").slideToggle()
@@ -6,4 +8,25 @@ $(document).ready(function(){
 
   const faqPanels = $("div[js-toggle='question']");
   faqPanels.each((_index, panel) => panel.addEventListener('click', accordionHandler))
+
+  $("button[js-event='contactUs']").click(function () {
+    $("div[js-view='modalOverlay']").show();
+    $('body').css('overflow','hidden');
+    $("div[js-view='contactUsModal']").slideDown();
+    activeModalContent = 'contactUsModal';
+  })
+  
+  $("button[js-event='applyToStart']").click(function () {
+    $("div[js-view='modalOverlay']").show();
+    $('body').css('overflow','hidden');
+    activeModalContent = 'createYourInitiativeModal';
+    $("div[js-view='createYourInitiativeModal']").slideDown();
+  })
+
+  $("button[js-event='closeModal']").click(function () {
+    $(`div[js-view='${activeModalContent}']`).slideUp();
+    $("div[js-view='modalOverlay']").hide();
+    $('body').css('overflow','auto');
+    activeModalContent = '';
+  })
 });
