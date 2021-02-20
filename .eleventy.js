b6583ca765c0ad1ac4acbd0219b0e7bd0e3581c5
Julian Boilen <julianboilen@gmail.com>
2021-02-20 13:18:26 -0500

    Sort sections by position data

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
-module.exports = function(eleventyConfig) {
-  
+const sortBy = require('lodash/sortBy');
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('_assets');
-  
+
+  eleventyConfig.addCollection('sections', function (collectionApi) {
+    // get unsorted items
+    const raw = collectionApi.getAll();
+    return sortBy(raw, 'data.position');
+  });
+
   return {
-    markdownTemplateEngine: "ejs",
+    markdownTemplateEngine: 'ejs',
     dir: {
-      layouts: '_layouts'
-    }
-  }
+      layouts: '_layouts',
+    },
+  };
 };
