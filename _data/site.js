4bccb16c2c98f1d5a7f9897737c93315cea5ffde
Julian Boilen <julianboilen@gmail.com>
2021-02-20 13:24:40 -0500

    Lint

--- _data/site.js
+++ _data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "title": "Solidarity not Charity - Grantmaking in the Solidarity Economy",
-  "publicPath": process.env.URL || 'http://localhost:8080'
+  title: 'Solidarity not Charity - Grantmaking in the Solidarity Economy',
+  publicPath: process.env.URL || 'http://localhost:8080',
 };
