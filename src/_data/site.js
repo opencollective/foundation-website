6cf4e9999f80bb23db558a6be70da8d7cf6ad48b
Julian Boilen <julianboilen@gmail.com>
2021-02-25 14:13:15 -0500

    First take at above-the-fold content

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,5 @@
 module.exports = {
   title: 'Solidarity not Charity - Grantmaking in the Solidarity Economy',
   publicPath: process.env.URL || 'http://localhost:8080',
+  environment: process.env.NODE_ENV,
 };
