b97465fc21d8e1756accf5aa9ea505cf4da8c266
Julian Boilen <julianboilen@gmail.com>
2021-03-02 22:30:52 -0500

    PDF interstitial V1

---
+++ src/assets/scripts/popup.js
@@ -0,0 +1,68 @@
+// Party like it's 2013 (I dont want to manage a transpiler)
+var HAS_SIGNED_UP = 'signed-up';
+
+(function () {
+  var popupEl = null;
+
+  function resetPopup() {
+    popupEl.removeAttribute('data-pdf-href');
+  }
+
+  function closePopup() {
+    var classList = popupEl.classList;
+    classList.add('hidden');
+    document.documentElement.classList.remove('noscroll');
+    resetPopup();
+  }
+
+  function showPdfInterstitial(pdfUrl) {
+    popupEl.classList.remove('hidden');
+    popupEl.setAttribute('data-pdf-href', pdfUrl);
+    document.documentElement.classList.add('noscroll');
+  }
+
+  function downloadPdf(pdfUrl) {
+    var link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = '';
+    link.dispatchEvent(new MouseEvent('click'));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    var pdfUrl = popupEl.getAttribute('data-pdf-href');
+    var formData = new FormData(e.target);
+    formData.append('pdf', pdfUrl);
+    var request = new XMLHttpRequest();
+    request.onload = function () {
+      downloadPdf(pdfUrl);
+    };
+    request.open('POST', '/');
+    request.send(formData);
+  }
+
+  function bind() {
+    document
+      .querySelectorAll('.scrim, .popup .close-button')
+      .forEach(function (el) {
+        el.addEventListener(
+          'click',
+          function (e) {
+            if (e.target !== this) return;
+            closePopup();
+          },
+          true
+        );
+      });
+
+    popupEl.querySelector('form').addEventListener('submit', handleSubmit);
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    popupEl = document.querySelector('.popup');
+    bind();
+    closePopup();
+  });
+
+  window.showPdfInterstitial = showPdfInterstitial;
+})();
---
+++ src/assets/scripts/registerInterstitials.js
@@ -0,0 +1,11 @@
+DEMO = true;
+document.addEventListener('DOMContentLoaded', function () {
+  const els = document.querySelectorAll('a[data-use-interstitial]');
+  els.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      if (!DEMO && Cookies.get('signed-up')) return true;
+      window.showPdfInterstitial(el.getAttribute('href'));
+      e.preventDefault();
+    });
+  });
+});
