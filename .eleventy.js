2517214bedd31b16be4194e42c1afa979e039aa5
Julian Boilen <julianboilen@gmail.com>
2021-07-10 15:57:42 -0400

    Only open absolute links in new window

--- .eleventy.js
+++ .eleventy.js
@@ -4,12 +4,15 @@ const md = require('markdown-it')('commonmark');
 const mila = require('markdown-it-link-attributes');
 const mic = require('markdown-it-container');
 
-md.use(mila, {
-  attrs: {
-    target: '_blank',
-    rel: 'noopener noreferrer',
+md.use(mila, [
+  {
+    pattern: /^https?:\/\//,
+    attrs: {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
   },
-});
+]);
 
 // Correspond to class names in markdown-containers.less
 md.use(mic, 'break-inside-avoid');
