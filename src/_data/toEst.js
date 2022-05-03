266ad39d4ba5264b445736c11a7e635876107d8c
Julian Boilen <julianboilen@gmail.com>
2022-05-03 15:49:07 -0400

    Convert art-coop website into OCF website
    Strip out all art-coop content to make bare framework

--- src/_data/toEst.js
+++ src/_data/toEst.js
@@ -1,9 +1,5 @@
 const { utcToZonedTime } = require('date-fns-tz');
 
-/**
- * Makes lodash available in ejs as _
- */
-
 module.exports = () => {
   return (date) => {
     return utcToZonedTime(date, 'America/New_York');
