10d1ef754ac825ac36bdc4ad969b1c0cdaeb6256
Julian Boilen <julianboilen@gmail.com>
2022-05-06 11:12:00 -0400

    extract collective slug to constant

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,6 +1,8 @@
 const apiV1 = require('../shared/OpenCollectiveApiV1');
 
+const COLLECTIVE_SLUG = 'foundation';
+
 module.exports = async () => {
-  const { data } = await apiV1.get('/foundation/events.json');
+  const { data } = await apiV1.get(`/${COLLECTIVE_SLUG}/events.json`);
   return data;
 };
