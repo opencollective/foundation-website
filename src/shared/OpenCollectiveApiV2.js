4aa191696bb7f5a6625a5f155e45102835a3ff69
Julian Boilen <julianboilen@gmail.com>
2022-05-06 12:56:35 -0400

    Make api key optional

--- src/shared/OpenCollectiveApiV2.js
+++ src/shared/OpenCollectiveApiV2.js
@@ -3,12 +3,8 @@ const { GraphQLClient } = require('graphql-request');
 const ENDPOINT = 'https://api.opencollective.com/graphql/v2/';
 const API_KEY = process.env.OPEN_COLLECTIVE_API_KEY;
 
-if (!API_KEY) {
-  console.error('Missing OC API V2 key');
-}
-
 module.exports = new GraphQLClient(ENDPOINT, {
   headers: {
-    'Api-Key': API_KEY,
+    'Api-Key': API_KEY ?? undefined,
   },
 });
