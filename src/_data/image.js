7b4d4edf67a37ff7842d8716956e39ddea47b803
Julian Boilen <julianboilen@gmail.com>
2022-06-23 14:42:18 -0400

    Compress hero images

---
+++ src/_data/image.js
@@ -0,0 +1,43 @@
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+
+module.exports = () =>
+  /**
+   * Create image HTML tag and compress images
+   * @param {Object} attributes - Attributes for img tag, including src
+   * @param {*} baseWidths Widths in css pixels to create
+   * @returns picture tag html
+   */
+  function image(attributes, baseWidths) {
+    const { src } = attributes;
+    // if it's a local path, resolve it
+    let filePath = src.startsWith('/')
+      ? path.resolve(__dirname, '..', src.substring(1))
+      : src;
+
+    // Take each base width and multiply it by supported pixel densities
+    const widths = baseWidths.flatMap((width) => [width, width * 2]);
+
+    // include full size version
+    widths.push(null);
+
+    const options = {
+      widths,
+      formats: ['webp', 'jpeg'],
+      outputDir: './_site/assets-compressed',
+      urlPath: '/assets-compressed/',
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src);
+        const name = path.basename(src, extension);
+
+        return `${name}-${width}w-${id}.${format}`;
+      },
+    };
+
+    // Create image in background (async not supported in data files)
+    Image(filePath, options);
+
+    let metadata = Image.statsSync(filePath, options);
+
+    return Image.generateHTML(metadata, attributes);
+  };
