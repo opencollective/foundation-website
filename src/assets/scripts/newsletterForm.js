1b56c814033e6cb0ceb49fc0d1ef0953aab0d021
Julian Boilen <julianboilen@gmail.com>
2022-10-09 10:26:12 -0400

    Submit form with js

---
+++ src/assets/scripts/newsletterForm.js
@@ -0,0 +1,25 @@
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+
+  const url = form.getAttribute('action');
+  const data = new FormData(form);
+
+  fetch(url, {
+    method: 'POST',
+    data,
+  }).then(function (resp) {
+    if (resp.ok) form.classList.add('submitted');
+  });
+
+  return false;
+}
+
+function bindSignupForm() {
+  const els = document.querySelectorAll('form.sign-up-form');
+  els.forEach(function (el) {
+    el.addEventListener('submit', handleFormSubmit);
+  });
+}
+
+bindSignupForm();
